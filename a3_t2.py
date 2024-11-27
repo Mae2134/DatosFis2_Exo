424fd5665dc9aad1d1a07aed87d3de80703f6c2c
Mae2134 <lanae_0@hotmail.com>
2024-11-27 15:04:07 -0300

    7

---
+++ a3_t2.py
@@ -0,0 +1,28 @@
+def Calculadora_Kepler(distancia, periodo):
+        # Periodo al cuadrado
+    if len(distancia) == len(periodo):
+        #T_2
+        T_2 = []
+        for i in range(len(distancia)):
+            c = (periodo[i]**2)
+            T_2.append(c)
+        print(f"El periodo al cuadrado es: \n{T_2}\n")
+
+        # a_3
+        A_3 = []
+        for i in range(len(distancia)):
+            c = (distancia[i]**3)
+            A_3.append(c)
+        print(f"la distancia al cubo es:\n")
+
+        # constante
+        K_inv = []
+        for i in range(len(distancia)):
+            c = (periodo[i]**2)/(A_3[i]**3)
+            K_inv.append(c)
+        
+        print(f"Las constantes individuales son: \n{K_inv}\n")
+        return T_2, A_3, K_inv
+    
+    else:
+        return "La longitud de datos no coinciden entre si."
